ee9ad17f7008ca4d712404f6d816e20df15b91c6
Lucas Di Caro <lucas@MacBook-Air.fibertel.com.ar>
2021-08-08 12:14:56 -0300

    subida about us en proceso

---
+++ client/src/components/aboutUs/styles.js
@@ -0,0 +1,18 @@
+import {makeStyles} from "@material-ui/core";
+
+const UseStyles = makeStyles(() => ({
+
+    listItem:{
+        paddingLeft: 45,
+        width:200,
+        height:'auto',
+        border:'2px solid gray',
+        display:'flex',
+        alignItems:'center',
+        boxShadow: '#5a5050 1px 1px 14px',
+        flexDirection: 'column',
+    }
+})
+)
+
+export default UseStyles;
