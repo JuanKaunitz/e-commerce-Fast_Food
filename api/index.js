4b80f5480e52046fa2152452b14732368c00bbee
Lucas Di Caro <lucas@MacBook-Air.fibertel.com.ar>
2021-08-12 11:21:18 -0300

    subida cambio indexÃ

--- api/index.js
+++ api/index.js
@@ -28,6 +28,13 @@ server.use(cors());
 //rutas de la server
 server.use('/',routers())
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 server.listen(PORT,()=>{
     console.log(`servidor corriendo en el puerto: ${PORT}`)
 })
