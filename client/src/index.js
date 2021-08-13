398f90ac6d9e1731a41c373d43c0da6ab190c260
Juan Kaunitz <jkaunitz@gmail.com>
2021-08-12 22:18:10 -0300

    esta version no sirve

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./Redux/store/store";
+import axios from "axios";
 import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { domain, clientId } from "../src/components/Authentication/octavio";
+import dotenv from "dotenv";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:5001";
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirectUri={window.location.origin}
-      >
-      <App />
-      </Auth0Provider>
+    <React.StrictMode>      
+      <App />      
     </React.StrictMode>
   </Provider>,
   document.getElementById("root")
