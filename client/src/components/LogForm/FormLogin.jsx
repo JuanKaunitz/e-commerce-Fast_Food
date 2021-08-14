aea29dbd5cd2937bc5fa77d3f7bfae31d0fac33f
octaviobissutti <octavio_bissutti@hotmail.com>
2021-08-14 12:02:51 -0300

    Borrar login que no sirve

--- client/src/components/LogForm/FormLogin.jsx
+++ client/src/components/LogForm/FormLogin.jsx
@@ -1,82 +1,160 @@
-import React, { useState} from 'react';
-import { useDispatch } from 'react-redux';
-import { authUser } from '../../Redux/actions/actions';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { authUser } from "../../Redux/actions/actions";
+import GoogleLogin from "react-google-login";
+// import { useGoogleLogin } from 'react-google-login'
+// import { GoogleLogout } from "react-google-login";
+// import { useGoogleLogout } from 'react-google-login'
 
 export function validate(input) {
-    let errors = {};
-    if (!input.email) {
-      errors.email = 'Email is required';
-    } else if (!/\S+@\S+\.\S+/.test(input.username)) {
-      errors.email = 'Email is invalid';
-    }
-      if (!input.password) {
-        errors.password = 'Password is required';
-      } else if (!/(?=.*[0-9])/.test(input.password)) {
-        errors.password = 'Password is invalid';
-      }
-  
-    return errors;
-  };
+  let errors = {};
+  if (!input.email) {
+    errors.email = "Email is required";
+  } else if (!/\S+@\S+\.\S+/.test(input.username)) {
+    errors.email = "Email is invalid";
+  }
+  if (!input.password) {
+    errors.password = "Password is required";
+  } else if (!/(?=.*[0-9])/.test(input.password)) {
+    errors.password = "Password is invalid";
+  }
 
-
-const FormLogin = () => {
-
-    const dispatch = useDispatch();
-    const [input, setInput] = useState({
-        email: '',
-        password: ''
-    });
-
-    const [errors, setErrors] = useState({});
-
-    
-    const handleInputChange = function(e) {
-        setInput({
-          ...input, 
-          [e.target.name]: e.target.value  
-        });
-    
-        setErrors(validate({
-          ...input,
-          [e.target.name]: e.target.value
-        }));
-      }
-
-
-      const handleSubmit =  (e) => {
-        e.preventDefault();
-        console.log(input);
-        dispatch(authUser(input));
-        setInput({
-         email: '',
-         password: ''
-        });
-
-    };
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-       <div>
-          <label>Email:</label>
-          <input className={`${errors.email && 'danger'}`}
-      type="email" name="email" onChange={handleInputChange} value={input.email} /> 
-    {errors.username && (
-      <p className="danger">{errors.email}</p>
-    )}
-       </div>
-       <div>
-        <label>Password:</label>
-        <input className={`${errors.password && 'danger'}`}
-           type="password" name="password" onChange={handleInputChange} value={input.password} />
-        {errors.password && (
-          <p className="danger">{errors.password}</p>
-       )}
-       </div>
-       <input type= "submit" value= "Submit"/>
-     </form>
-        </div>
-    )
+  return errors;
 }
 
-export default FormLogin
+const FormLogin = () => {
+  const dispatch = useDispatch();
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
+  
+  
+
+  // const { signIn, loaded } = useGoogleLogin({
+  //   onSuccess,
+  //   onAutoLoadFinished,
+  //   clientId,
+  //   cookiePolicy,
+  //   loginHint,
+  //   hostedDomain,
+  //   autoLoad,
+  //   isSignedIn,
+  //   fetchBasicProfile,
+  //   redirectUri,
+  //   discoveryDocs,
+  //   onFailure,
+  //   uxMode,
+  //   scope,
+  //   accessType,
+  //   responseType,
+  //   jsSrc,
+  //   onRequest,
+  //   prompt
+  // })
+
+
+  // const { signOut, loaded } = useGoogleLogout({
+  //   jsSrc,
+  //   onFailure,
+  //   clientId,
+  //   cookiePolicy,
+  //   loginHint,
+  //   hostedDomain,
+  //   fetchBasicProfile,
+  //   discoveryDocs,
+  //   uxMode,
+  //   redirectUri,
+  //   scope,
+  //   accessType,
+  //   onLogoutSuccess
+  // })
+
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = function (e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    dispatch(authUser(input));
+    setInput({
+      email: "",
+      password: "",
+    });
+  };
+
+  const responseGoogle = (response) => {
+    console.log(response);
+    // history.push('/home')
+    console.log(response.profileobj);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            className={`${errors.email && "danger"}`}
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+            value={input.email}
+          />
+          {errors.username && <p className="danger">{errors.email}</p>}
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            className={`${errors.password && "danger"}`}
+            type="password"
+            name="password"
+            onChange={handleInputChange}
+            value={input.password}
+          />
+          {errors.password && <p className="danger">{errors.password}</p>}
+        </div>
+        <input type="submit" value="Submit" />
+        <br /> <br />
+          
+            <div className="Google-login">
+          <GoogleLogin
+            clientId="580821821792-1e9i4f7oh9bps02f5qoqs9b0svvur83c.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={"single_host_origin"}
+            isSignedIn={false}
+            
+          />
+          </div>
+          
+          {/* <div className = "Google-logout">
+            <GoogleLogout
+              clientId="580821821792-1e9i4f7oh9bps02f5qoqs9b0svvur83c.apps.googleusercontent.com"
+              buttonText="Logout"
+              onLogoutSuccess={responseGoogle}
+              isSignedIn={true}
+              onLogoutSuccess={logout}
+              />
+          </div>
+           */}
+      </form>
+    </div>
+  );
+};
+
+export default FormLogin;
--- client/src/components/LogForm/FormRegister.jsx
+++ client/src/components/LogForm/FormRegister.jsx
@@ -1,98 +1,117 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { newUser } from '../../Redux/actions/actions'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { newUser } from "../../Redux/actions/actions";
+import { Link } from "react-router-dom";
+
 
 export function validate(input) {
-    let errors = {};
-    if (!input.email) {
-      errors.email = 'Email is required';
-    } else if (!/\S+@\S+\.\S+/.test(input.email)) {
-      errors.email = 'Email is invalid';
-    }
-      if (!input.password) {
-        errors.password = 'Password is required';
-      } else if (!/(?=.*[0-9])/.test(input.password)) {
-        errors.password = 'Password is invalid';
-      }
-      if (!input.name) {
-        errors.name = 'Name is required';
-      } else if (!/^[A-Za-z]+$/.test(input.name)) {
-        errors.name = 'Name must be a string!';
-      }
-  
-    return errors;
-  };
+  let errors = {};
+  if (!input.email) {
+    errors.email = "Email is required";
+  } else if (!/\S+@\S+\.\S+/.test(input.email)) {
+    errors.email = "Email is invalid";
+  }
+  if (!input.password) {
+    errors.password = "Password is required";
+  } else if (!/(?=.*[0-9])/.test(input.password)) {
+    errors.password = "Password is invalid";
+  }
+  if (!input.name) {
+    errors.name = "Name is required";
+  } else if (!/^[A-Za-z]+$/.test(input.name)) {
+    errors.name = "Name must be a string!";
+  }
 
-const FormRegister = () => {
-
-    const dispatch = useDispatch();
-
-    const [input, setInput] = useState({
-        name: '',
-        email: '',
-        password: '',
-    });
-
-    const [errors, setErrors] = useState({});
-
-    const handleInputChange = function(e) {
-        setInput({
-          ...input, 
-          [e.target.name]: e.target.value  
-        });
-    
-        setErrors(validate({
-          ...input,
-          [e.target.name]: e.target.value
-        }));
-      }
-
-      
-      const handleSubmit =  (e) => {
-        e.preventDefault();
-        console.log(input);
-        dispatch(newUser(input));
-        setInput({
-         name: '',
-         email: '',
-         password: ''
-        });
-
-    };
-
-
-    return (
-        <div>
-             <form onSubmit={handleSubmit}>
-
-             <div>
-          <label>Name:</label>
-          <input className={`${errors.name && 'danger'}`}
-      type="name" name="name" onChange={handleInputChange} value={input.name} /> 
-    {errors.name && (
-      <p className="danger">{errors.name}</p>
-    )}
-       </div>          
-       <div>
-          <label>Email:</label>
-          <input className={`${errors.email && 'danger'}`}
-      type="email" name="email" onChange={handleInputChange} value={input.email} /> 
-    {errors.username && (
-      <p className="danger">{errors.email}</p>
-    )}
-       </div>
-       <div>
-        <label>Password:</label>
-        <input className={`${errors.password && 'danger'}`}
-           type="password" name="password" onChange={handleInputChange} value={input.password} />
-        {errors.password && (
-          <p className="danger">{errors.password}</p>
-       )}
-       </div>
-       <input type= "submit" value= "Submit"/>
-     </form>
-        </div>
-    )
+  return errors;
 }
 
-export default FormRegister
+const FormRegister = () => {
+  const dispatch = useDispatch();
+
+  const [input, setInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = function (e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    dispatch(newUser(input));
+    setInput({
+      name: "",
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div className="form-content-right">
+      <form onSubmit={handleSubmit} className="form">
+        <h1>
+          Get started with us today! Create your account by filling out the
+          information below.
+        </h1>
+
+        <div className="form-inputs">
+          <label className="form-label">Name:</label>
+          <input
+            className="form-input"            
+            type="name"
+            name="name"
+            onChange={handleInputChange}
+            value={input.name}
+          />
+          {errors.name && <p className="danger">{errors.name}</p>}
+        </div>
+
+        <div className="form-inputs">
+          <label className="form-label">Email:</label>
+          <input
+            className="form-input"
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+            value={input.email}
+          />
+          {errors.username && <p className="danger">{errors.email}</p>}
+        </div>
+
+        <div className="form-inputs">
+          <label className="form-label">Password:</label>
+          <input
+            className="form-input"
+            type="password"
+            name="password"
+            onChange={handleInputChange}
+            value={input.password}
+          />
+          {errors.password && <p className="danger">{errors.password}</p>}
+        </div>
+        <button type="submit" className='form-input-btn'> Submit </button>
+        <br></br>
+        <span className='form-input-login'>
+          Already have an account? Login <Link to='/register'>here</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default FormRegister;
