49e628ddc9fa9d40179c72f9529c3a447d7660d6
Juan Kaunitz <jkaunitz@gmail.com>
2021-08-15 21:01:51 -0300

    CRUD CATEGORIAS funcional, falta renderizar los tipos creados

--- api/index.js
+++ api/index.js
@@ -5,43 +5,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const { URL_CONNECTION, DB ,PORT} = process.env;
 
-//conectar a mongoose
-// let Mongoose =
-//   process.env.NODE_ENV === "production"
-//     ? new mongoose({
-//         database: "fastfood",
-//         dialect: "postgres",
-//         host: "localhost",
-//         port: 5432,
-//         username: "duartes",
-//         password: "24090512",
-//         pool: {
-//           max: 3,
-//           min: 1,
-//           idle: 10000,
-//         },
-//         dialectOptions: {
-//           ssl: {
-//             require: true,
-//             // Ref.: https://github.com/brianc/node-postgres/issues/2009
-//             rejectUnauthorized: false,
-//           },
-//           keepAlive: true,
-//         },
-//         ssl: true,
-//       })
-//     : new mongoose.connect(`${URL_CONNECTION}${DB}`, {
-//         useNewUrlParser:true,
-//         useUnifiedTopology:true,
-//         useCreateIndex:true,
-//         useFindAndModify:false
-//     }, () => {
-//         console.log('DataBase connected')
-//     });
-
-
 mongoose.Promise = global.Promise;
-mongoose.connect(`${URL_CONNECTION}`, {
+mongoose.connect(`${URL_CONNECTION}${DB}`, {
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true,
@@ -62,7 +27,7 @@ server.use(cors());
 server.use('/',routers())
 
 server.listen(PORT ,()=>{
-    console.log(`servidor corriendo en el puerto: `)
+    console.log(`servidor corriendo en el puerto: ${PORT}`)
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
@@ -71,20 +36,3 @@ server.use((req, res, next) => {
   });
 
 })
-//MIDDLEWARES
-
-// server.use(multer({
-// dest: path.join(_dirname,"destino") //destino donde quiero que vuelvan las imagenes
-
-
-// }).singel("imput_delimage"))
-
-
-
-// server.post('/upload' , (req,res)=>{
-
-
-// req.file // objeto con prop de la imagen 
-// res.send("subida")
-
-// })
