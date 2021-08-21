ee9583de7bbe96c12e62f18848607965919871ce
octaviobissutti <octavio_bissutti@hotmail.com>
2021-08-21 10:44:52 -0300

    Limpieza de contador del carrito y borrar productos en localStorage cuando se desloguea

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import AboutUs from "./components/aboutUs/AboutUs.jsx";
 import Cart from "./components/cart/Cart.jsx";
 import Home from "./components/Home/Home.jsx";
-import Navbar from "./components/Navbar/Navbar.jsx";
+import { Navbar } from "./components/Navbar/Navbar.jsx";
 import Register from "./components/register/Register.jsx";
 import CardDetails from "./components/details/CardDetails.jsx";
 import Categories from "./components/categories/Categories.jsx";
