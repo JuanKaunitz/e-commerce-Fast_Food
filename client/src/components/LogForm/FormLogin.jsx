5a7f4c3c6ac859a84090410a78918a89ca28e6ab
octaviobissutti <octavio_bissutti@hotmail.com>
2021-08-25 16:46:29 -0300

    Formulario register completo con estilos, alerta en register y login

--- client/src/components/LogForm/FormLogin.jsx
+++ client/src/components/LogForm/FormLogin.jsx
@@ -5,8 +5,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -14,6 +12,9 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { useHistory } from "react-router";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 
 export function validate(input) {
@@ -57,6 +58,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FormularioLogin() {
   const dispatch = useDispatch();  
+  const history = useHistory();
   const [input, setInput] = useState({
     email: "",
     password: "",
@@ -78,16 +80,34 @@ export default function FormularioLogin() {
       })
     );
   };
+
+  const MySwal = withReactContent(Swal)
+
+  const showAlert =  () => {
+    MySwal.fire({
+      didOpen: () => {
+        MySwal.clickConfirm()
+      }
+    }).then(() => {
+      return MySwal.fire({
+        title: 'Welcome!',
+        button: 'Aceptar',
+        icon: 'success'
+      })
+    })
+  }
+
   
   const handleSubmit =  (e) => {
     e.preventDefault();
+    showAlert();
     console.log(input);
     dispatch(authUser(input));
     setInput({
       email: "",
       password: "",
      });
-    //history.push('/')
+     history.push("/");
   };
 
   const classes = useStyles();
@@ -134,10 +154,6 @@ export default function FormularioLogin() {
           />
           {errors.password && <p className={classes.danger}>{errors.password}</p>}
 
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
           <Button
             type="submit"
             fullWidth
@@ -155,7 +171,7 @@ export default function FormularioLogin() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/register" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
--- client/src/components/LogForm/FormRegister.jsx
+++ client/src/components/LogForm/FormRegister.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { newUser, createGoogleUser } from "../../Redux/actions/actions";
-import styles from "./styles.module.css";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { useHistory } from "react-router";
 
 export function validate(input) {
   let errors = {};
@@ -14,45 +24,47 @@ export function validate(input) {
   }
   if (!input.password) {
     errors.password = "Password is required";
-  } else if (!/(?=.*[0-9])/.test(input.password)) { 
+  } else if (!/(?=.*[0-9])/.test(input.password)) {
     errors.password = "Password is invalid";
-  } else if(!/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/.test(input.password)) {
-    errors.password = "Password requires 8-16 digits, at least one digit and one upper case."
+  } else if (
+    !/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/.test(input.password)
+  ) {
+    errors.password =
+      "Password requires 8-16 digits, at least one digit and one upper case.";
   }
   if (!input.name) {
     errors.name = "Name is required";
   } else if (!/^[A-Za-z]+$/.test(input.name)) {
     errors.name = "Name must be a string!";
   }
-  if(input.password !== input.password2) {
+  if (input.password !== input.password2) {
     errors.password2 = "The password doesn´t match!";
   }
   return errors;
 }
 
-const FormRegister = () => {
+export default function SignInSide() {
   const dispatch = useDispatch();
-  
+  const history = useHistory();
+
   const [input, setInput] = useState({
     name: "",
     email: "",
     password: "",
-    password2: ""
+    password2: "",
   });
-  
-  
-  
+
   const [errors, setErrors] = useState({});
-  
+
   const [showloginButton, setShowloginButton] = useState(true);
   const [showlogoutButton, setShowlogoutButton] = useState(false);
-  
+
   const handleInputChange = function (e) {
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
-    
+
     setErrors(
       validate({
         ...input,
@@ -61,22 +73,39 @@ const FormRegister = () => {
     );
   };
 
+  const MySwal = withReactContent(Swal);
+
+  const showAlert = () => {
+    MySwal.fire({
+      didOpen: () => {
+        MySwal.clickConfirm();
+      },
+    }).then(() => {
+      return MySwal.fire({
+        title: "Successful registration",
+        button: "Aceptar",
+        icon: "success",
+      });
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(newUser(input));
+    showAlert();
     setInput({
       name: "",
       email: "",
       password: "",
     });
-    window.location.replace("http://localhost:3000/register");
+    history.push("/login");
   };
   const responseGoogle = (response) => {
-    console.log(response)
+    console.log(response);
   };
 
   const logout = () => {
-    alert("usted esta saliendo de su cuenta");
+    alert("Usted esta saliendo de su cuenta");
     console.clear();
     sessionStorage.clear();
     setShowloginButton(true);
@@ -94,94 +123,148 @@ const FormRegister = () => {
     console.log("Login Failed:", res);
   };
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    paper: {
+      margin: theme.spacing(10),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 1),
+    },
+    link: {
+      cursor: "pointer",
+    },
+    danger: {
+      color: "rgb(231, 14, 14)",
+      fontWeight: "bold",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <text className={styles.texto}>Formulario de Registro</text>
-        <div>
-        <br></br>
-          <label className={styles.label}>Name:</label>
-          <input
-            className={styles.input1}
-            type="name"
-            name="name"
-            class={styles.field}
-            onChange={handleInputChange}
-            value={input.name}
-            required
-          />
-          {errors.name && <p className="danger">{errors.name}</p>}
-        </div>
-        <div>
-          <label className={styles.label}>Email:</label>
-          <input
-            className={styles.input1}
-            type="email"
-            class={styles.field1}
-            name="email"
-            onChange={handleInputChange}
-            value={input.email}
-            required
-          />
-          {errors.username && <p className="danger">{errors.email}</p>}
-        </div>
-        <div>
-          <label className={styles.label}>Password:</label>
-          <input
-            className={styles.input1}
-            type="password"
-            name="password"
-            class={styles.field2}
-            onChange={handleInputChange}
-            value={input.password}
-            required
-          />
-          {errors.password && <p className="danger">{errors.password}</p>}
-        </div>
-
-        <div>
-          <label className={styles.label}>Repeat password:</label>
-          <input
-            className={styles.input1}
-            type="password"
-            name="password2"
-            class={styles.field2}
-            onChange={handleInputChange}
-            value={input.password2}
-          />
-          {errors.password2 && <p className="danger">{errors.password2}</p>}
-        </div>
-
-        <div className="btn submit">
-          <button className={styles.btnregister} type="submit" src="/">
-            Submit
-          </button>
-
-          {showloginButton ? (
-            <GoogleLogin
-              clientId="371009516574-6nj2o8vbfdtom1lafa91d55scq54fm9u.apps.googleusercontent.com"
-              buttonText="Login"
-              onSuccess={onLoginSuccess}
-              onFailure={onLoginFailure}
-              cookiePolicy={"single_host_origin"}
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} elevation={6} square>
+        <div className={classes.paper}>
+          <br></br>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+              onChange={(e) => handleInputChange(e)}
             />
-          ) : null}
-          {showlogoutButton ? (
-            <GoogleLogout
-              clientId="371009516574-6nj2o8vbfdtom1lafa91d55scq54fm9u.apps.googleusercontent.com"
-              buttonText="Logout"
-              onFailure={responseGoogle}
-              onLogoutSuccess={logout}
+            {errors.name && <p className={classes.danger}>{errors.name}</p>}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(e) => handleInputChange(e)}
             />
-          ) : null}
+            {errors.email && <p className={classes.danger}>{errors.email}</p>}
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(e) => handleInputChange(e)}
+            />
+            {errors.password && (
+              <p className={classes.danger}>{errors.password}</p>
+            )}
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Repeat Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(e) => handleInputChange(e)}
+            />
+            {/* {errors.password2 && <p className="danger">{errors.password2}</p>} */}
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Register
+            </Button>
+            <div>
+              {showloginButton ? (
+                <GoogleLogin
+                  clientId="371009516574-6nj2o8vbfdtom1lafa91d55scq54fm9u.apps.googleusercontent.com"
+                  buttonText="Login"
+                  onSuccess={onLoginSuccess}
+                  onFailure={onLoginFailure}
+                  cookiePolicy={"single_host_origin"}
+                />
+              ) : null}
+              {showlogoutButton ? (
+                <GoogleLogout
+                  clientId="371009516574-6nj2o8vbfdtom1lafa91d55scq54fm9u.apps.googleusercontent.com"
+                  buttonText="Logout"
+                  onFailure={responseGoogle}
+                  onLogoutSuccess={logout}
+                />
+              ) : null}
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <span className={classes.link}>
+              Already have an account? Login <Link to="/login">here</Link>
+            </span>
+          </form>
         </div>
-        <br></br>
-        <span className="form-input-login">
-          Already have an account? Login <Link to="/register">here</Link>
-        </span>
-      </form>
-    </div>
+      </Grid>
+    </Grid>
   );
-};
-
-export default FormRegister;
+}
