1bdcb445a0e32918eb22fb795b19aa58290551cd
octaviobissutti <octavio_bissutti@hotmail.com>
2021-08-26 15:27:04 -0300

    Estilos Home, Carrusel, ChatBot, About Us

--- client/src/components/aboutUs/styles.js
+++ client/src/components/aboutUs/styles.js
@@ -1,36 +1,50 @@
-import {makeStyles} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 
 const UseStyles = makeStyles(() => ({
-
-    listItem:{
-        paddingLeft: 40,
-        width:250,
-        /* height:'auto', */
-        border:'2px solid gray',
-        display:'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(min(100%, 30rem), 1fr))',
-        alignItems:'center',
-        boxShadow: '#5a5050 1px 1px 14px', 
-        webkitBorderRadius: '40px 0px 40px 40px',
-        mozBorderRadius: '40px 0px 40px 40px',
-        borderRadius: '40px 0px 40px 40px',
-        marginBottom: '50px',
-        textDecoration: 'none',
-        backgroundColor: 'white'
-       /*  flexDirection: 'column', */
+  listItem: {
+    paddingLeft: 40,
+    width: 250,
+    /* height:'auto', */
+    border: "2px solid gray",
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(min(100%, 30rem), 1fr))",
+    alignItems: "center",
+    boxShadow: "#5a5050 1px 1px 14px",
+    webkitBorderRadius: "40px 0px 40px 40px",
+    mozBorderRadius: "40px 0px 40px 40px",
+    borderRadius: "40px 0px 40px 40px",
+    marginBottom: "50px",
+    textDecoration: "none",
+    backgroundColor: "white",
+    "& a" : {
+      listStyle: "none",
+      textDecoration: "none",
+      color: "blue",
+      fontSize: "20px",
     },
-    cards:{
-        display:'flex',
-        /* flexDirection:'column', */
-        maxWidth: '100% ',
-      /*   marginRight: 10, */
-       /*  justifyContent: 'space-between',  */
-        gridTemplateColumns: 'repeat(auto-fill, minmax(min(100%, 30rem), 1fr))'
-    },
-    color:{
-        color:"white"
-      }
-})
-)
+    /*  flexDirection: 'column', */
+  },
+  cards: {
+    display: "flex",
+    /* flexDirection:'column', */
+    maxWidth: "100% ",
+    /*   marginRight: 10, */
+    /*  justifyContent: 'space-between',  */
+    gridTemplateColumns: "repeat(auto-fill, minmax(min(100%, 30rem), 1fr))",
+    textDecoration: "none",
+  },
+  color: {
+    fontWeight: "bold",
+    fontSize: "20px",
+    // letterSpacing: '1',
+    width: "80%",
+    content: "justify",
+    color: "black",
+  },
+  title: {
+      color: 'black',
+      textAlign: 'center'
+  }
+}));
 
 export default UseStyles;
